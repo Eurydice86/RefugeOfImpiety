7d8d22b833401c53da5bb1b673c9ad1bea454e09
Nikos Kyriakopoulos <37345138+Eurydice86@users.noreply.github.com>
2021-09-07 18:06:29 +0300

    Slow outro changes
    outro_slow changed to feel more dreamlike: added blur, added "lightning strikes", removed the bricks background, made it into an empty void, with black ground

--- assets/levels/outro_slow.gd
+++ assets/levels/outro_slow.gd
@@ -2,6 +2,9 @@ extends Node2D
 
 export (int) var bpm = 186
 export var start = 232.6
+var blur = false
+var alpha = 0
+var alphaDecay = 0.1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,8 +13,13 @@ func _ready():
 	$CanvasModulate.visible = false
 
  #Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
+func _process(delta):
 	$CanvasModulate.color = Global.indoorsLightColor
+	if blur:
+		$WorldEnvironment.environment.dof_blur_near_amount = 0.2 * randf()
+	if alpha > 0:
+		alpha -= alphaDecay * delta
+	$ColorRect.color = Color(1,1,1,alpha)
 
 func _on_Timer_timeout():
 	$player.walking = true
@@ -21,7 +29,10 @@ func _on_startRunning_body_entered(body):
 	if body.name[0] == "p":
 		$player.walking = false
 		$player.running = true
+		blur = false
+		$WorldEnvironment.environment.dof_blur_near_amount = 0
 		$CanvasModulate.visible = false
+		
 
 
 func _on_stop_moving_body_entered(body):
@@ -29,3 +40,12 @@ func _on_stop_moving_body_entered(body):
 	body.running = false
 	$stop_moving/Timer.start()
 	$CanvasModulate.visible = true
+	blur = true
+	$ground.visible = true
+	$ColorRect.visible = true
+	alpha = 1
+
+
+func _on_lightningStrike_body_entered(body):
+	alpha = 1
+	alphaDecay = 0.9
