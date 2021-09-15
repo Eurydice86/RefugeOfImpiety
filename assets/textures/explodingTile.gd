0db0f344483bd60b3c56b4c6a92c7b01a369aa4e
Nikos Kyriakopoulos <37345138+Eurydice86@users.noreply.github.com>
2021-09-15 21:53:08 +0300

    fixed roaring

--- assets/textures/explodingTile.gd
+++ assets/textures/explodingTile.gd
@@ -1,11 +1,19 @@
 extends Particles2D
 
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 export (Resource) var image
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Sprite.texture = image
+<<<<<<< Updated upstream
 	print(get_material().shader)
+=======
+
+>>>>>>> Stashed changes
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
