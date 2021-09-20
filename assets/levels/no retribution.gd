63fc5892fa0fbda50be64672ec6a6a5d9d55be6b
Nikos Kyriakopoulos <37345138+Eurydice86@users.noreply.github.com>
2021-09-20 18:53:53 +0300

    level improvements
    finalized (?) layout for verse1, chorus 1, verse2

---
+++ assets/levels/no retribution.gd
@@ -0,0 +1,21 @@
+extends RichTextLabel
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var appearSpeed = 0.8
+var appear = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	percent_visible = 0
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if appear:
+		percent_visible += appearSpeed * delta
+
+func _on_textAppear_body_entered(body):
+	if body.name == "player":
+		appear = true
--- assets/levels/verse2.gd
+++ assets/levels/verse2.gd
@@ -1,15 +1,16 @@
 extends Node2D
 
 var bpm = Global.bpm
-export var start = 109.5
+export var start = 109
 var alpha = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	
 	# remove the next two lines for a change from soaring to this
-	#Global.audio.seek(start)
-	#Global.audio.stream_paused = false
+	# move player 7 tiles forward
+	Global.audio.seek(start)
+	Global.audio.stream_paused = false
 	$ColorRect.color = Color(1, 1, 1, alpha)
 
 func _process(delta):
