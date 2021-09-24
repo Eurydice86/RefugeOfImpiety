36dc4071c025ff69d8e3619800e905235153e00a
Nikos Kyriakopoulos <37345138+Eurydice86@users.noreply.github.com>
2021-09-25 00:51:12 +0300

    cave etc
    cave added to outro_slow, removed blur / dreamy

--- assets/levels/bridge2.gd
+++ assets/levels/bridge2.gd
@@ -7,6 +7,11 @@ export var start = 207
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-#	Global.audio.seek(start)
-#	Global.audio.stream_paused = false
+	Global.audio.seek(start)
+	Global.audio.stream_paused = false
 	pass
+
+
+func _on_change_scene_body_entered(body):
+	if body.name == "player":
+		get_tree().change_scene("res://assets/levels/outro_slow.tscn")
