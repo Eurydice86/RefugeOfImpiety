295ba1b67e8cde5fa734482c9b1979c0aeae79ae
Nikos Kyriakopoulos <37345138+Eurydice86@users.noreply.github.com>
2021-09-25 12:14:48 +0300

    outro improvements

--- assets/textures/fallingTile.gd
+++ assets/textures/fallingTile.gd
@@ -10,11 +10,6 @@ func _ready():
 	sleeping = true
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
 func _on_Area2D_body_exited(body):
 	if body.name[0] == "p":
 		sleeping = false
