8840e6e215258df17a48566fc0d15802065f08f0
Nikos Kyriakopoulos <37345138+Eurydice86@users.noreply.github.com>
2021-12-10 00:57:12 +0200

    bridge2 text added

--- assets/levels/bridge2.gd
+++ assets/levels/bridge2.gd
@@ -7,11 +7,14 @@ export var start = 207.2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-#	Global.audio.seek(start)
-#	Global.audio.stream_paused = false
+	#Global.audio.seek(start)
+	#Global.audio.stream_paused = false
 	pass
 
 
 func _on_change_scene_body_entered(body):
 	if body.name == "player":
-		get_tree().change_scene("res://Part4.tscn")
+		if get_tree().change_scene("res://Part4.tscn") != OK:
+			print ("An unexpected error occured when trying to switch to the Readme scene")
+		else:
+			get_tree().change_scene("res://Part4.tscn")
