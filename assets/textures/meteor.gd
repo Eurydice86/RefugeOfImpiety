c711c154187882ebf064e212c9d6613ad07b4806
Nikos Kyriakopoulos <37345138+Eurydice86@users.noreply.github.com>
2022-10-23 02:13:56 +0300

    habemus meteoris

--- assets/textures/meteor.gd
+++ assets/textures/meteor.gd
@@ -5,32 +5,36 @@ export var rnd : bool = true
 export var speed = 2000
 export var speedMin = 1000
 export var speedMax = 2000
-var scaleFactor
 
 var init_position
+var init_speed
+var init_time
+var init_size
 
-func init(pos, scaleF):
+func init(pos, ispeed, itime, isize):
+	init_speed = ispeed
+	init_time = itime
 	init_position = pos
-	scaleFactor = 0.1 * scaleF
+	init_size = isize
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$Timer.wait_time = init_time
+	$Timer.start()
 	position = init_position
-	var scaleMult = randf()
-	$AnimatedSprite.scale =  scaleFactor * scaleMult * Vector2(1, 1).normalized()
-	$VisibilityEnabler2D.rect = Rect2( -524, 524, 1000, -1000)
-	if rnd:
-		speed = scaleFactor * (speedMin + randi() % (speedMax-speedMin))
+	speed = init_speed
 	velocity = Vector2(-1, 1).normalized()
-
-
+	scale = Vector2(init_size,init_size)
+	$Particles2D.amount = 800*init_size
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	move_and_collide(speed * delta * velocity)
+	$Particles2D.process_material.set("scale", init_size)
+
 
 func _draw():
 	Rect2(-524, 524, 1000, -1000)
 
-func _on_VisibilityEnabler2D_screen_exited():
-	print("out")
+func _on_Timer_timeout():
 	queue_free()
