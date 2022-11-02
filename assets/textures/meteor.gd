34d89ae1d2ee260442382760072d0aa9b8d53e67
Nikos Kyriakopoulos <37345138+Eurydice86@users.noreply.github.com>
2022-11-02 21:45:53 +0200

    meteors and visual improvements

--- assets/textures/meteor.gd
+++ assets/textures/meteor.gd
@@ -26,21 +26,20 @@ func _ready():
 	speed = init_speed
 	velocity = Vector2(-1, 1).normalized()
 	scale = Vector2(init_size,init_size)
-	$Particles2D.amount = 800*init_size
+	$Particles2D.amount = 512*init_size
+	#$Particles2D.process_material.set("scale", init_size)
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	move_and_collide(speed * delta * velocity)
-	$Particles2D.process_material.set("scale", init_size)
-
-
+	
 func _on_Timer_timeout():
 	queue_free()
-
 
 func _on_Area2D_body_entered(body):
 	if body.name == "TileMap":
 		explosion()
 
 func explosion():
+	$AnimatedSprite.position = Vector2(-24,24)
 	$AnimatedSprite.play("explosion")
