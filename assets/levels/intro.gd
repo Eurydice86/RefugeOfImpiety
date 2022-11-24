b6209f6a0c79cae185dfdfdf4349b7e723c8bf10
Nikos Kyriakopoulos <37345138+Eurydice86@users.noreply.github.com>
2022-11-24 23:23:14 +0200

    polishing and changes
    retract bridge, add tilesets, etc

--- assets/levels/intro.gd
+++ assets/levels/intro.gd
@@ -5,5 +5,12 @@ export var start = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Global.audio.seek(start)
-	Global.audio.stream_paused = false
+	pass
+
+
+
+
+func _on_Area2D_body_entered(body):
+	if "player" in body.name:
+		Global.audio.play()
+
